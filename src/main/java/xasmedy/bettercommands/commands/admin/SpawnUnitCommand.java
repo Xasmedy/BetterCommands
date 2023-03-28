4aced907cf63bee2d615038e094dce0d9490f19b
xasmedy <xasmedy08cim@gmail.com>
2023-03-28 20:34:46 +0200

    Finished the unit spawn code. (But not the spawn command :eyes:)

--- src/main/java/xasmedy/bettercommands/commands/admin/SpawnUnitCommand.java
+++ src/main/java/xasmedy/bettercommands/commands/admin/SpawnUnitCommand.java
@@ -13,7 +13,9 @@ import arc.util.CommandHandler;
 import arc.util.Strings;
 import mindustry.Vars;
 import mindustry.content.UnitTypes;
+import mindustry.entities.Units;
 import mindustry.game.EventType;
+import mindustry.game.Team;
 import mindustry.gen.Call;
 import mindustry.gen.Player;
 import mindustry.gen.Unit;
@@ -21,7 +23,9 @@ import mindustry.type.UnitType;
 import mindustry.ui.Menus;
 import xasmedy.bettercommands.Util;
 import xasmedy.bettercommands.commands.Command;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Optional;
 import java.util.stream.StreamSupport;
 import static xasmedy.bettercommands.Util.PREFIX;
 
@@ -29,7 +33,11 @@ public class SpawnUnitCommand implements Command {
 
     private static final String UNIT_NOT_FOUND_ERROR = "%s[scarlet]Could not find the unit [orange]%s[].";
     private static final String INVALID_SPAWN_ERROR = "%s[scarlet]The unit [orange]%s[] cannot be spawned at this location.";
-    private static final String UNIT_SPAWNED_MESSAGE = "%s[orange]The unit [sky]%s[] has been spawned. [gray]([sky]%s[accent]/[sky]%d[gray])";
+    private static final String INVALID_AMOUNT_ERROR = "%s[scarlet]The amount [orange]%s[] is not valid.";
+    private static final String INVALID_TEAM_ERROR = "%s[scarlet]The team [orange]%s[] does not exist.";
+    private static final String INVALID_SHIELD_ERROR = "%s[scarlet]The shield value [orange]%s[] is not valid.";
+    private static final String UNIT_SPAWNED_MESSAGE = "%s[orange]The unit [#%s]%s[] has been spawned [sky]%d[] time%s. [gray]([sky]%s[accent]/[sky]%d[gray])";
+    private static final String UNIT_NOT_ALL_SPAWNED_MESSAGE = "%s[orange]The unit [#%s]%s[] has been spawned [sky]%d[accent]/[sky]%d[orange] times. [gray]([sky]%s[accent]/[sky]%d[gray])";
     private static final String UNIT_CAP_REACHED_MESSAGE = "%s[red]The cap of [sky]%d[] has been reached for the unit [sky]%s[red].";
     private final HashMap<Player, ListMenu> activeMenus = new HashMap<>();
     private UnitType[] availableUnits = null;
@@ -57,32 +65,113 @@ public class SpawnUnitCommand implements Command {
         Events.on(EventType.PlayerJoin.class, e -> activeMenus.remove(e.player));
     }
 
-    private void spawnUnit(UnitType type, Player admin) {
+    private float getShieldFromInput(String rawShield, Player admin) {
 
-        // I try to spawn the unit.
-        final Unit unit = type.spawn(admin.x, admin.y);
+        // Special exception.
+        if (rawShield.equalsIgnoreCase("max")) return Float.MAX_VALUE;
 
-        // If the unit is dead, it means it could not spawn because of the cap.
+        try {
+            final float shield = Float.parseFloat(rawShield);
+            if (shield < 0) throw new NumberFormatException("Negative number.");
+            return shield;
+        } catch (NumberFormatException ignored) {
+            final String message = String.format(INVALID_SHIELD_ERROR, PREFIX, rawShield);
+            admin.sendMessage(message);
+            return -1;
+        }
+    }
+
+    private Optional<Team> findTeam(String rawTeam) {
+        // I allow all teams.
+        for (Team team : Team.all) {
+            if (!team.name.equalsIgnoreCase(rawTeam)) continue;
+            return Optional.of(team);
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * -1 if not a valid amount.
+     */
+    private int getAmountFromInput(String rawAmount, Player admin) {
+        try {
+            final int number = Integer.parseInt(rawAmount);
+            if (number < 1) throw new NumberFormatException("Negative number.");
+            return number;
+        } catch (NumberFormatException ignored) {
+            final String message = String.format(INVALID_AMOUNT_ERROR, PREFIX, rawAmount);
+            admin.sendMessage(message);
+            return -1;
+        }
+    }
+
+    private void sendSpawnMessage(ArrayList<Unit> spawned, Player admin, UnitType type, Team team, int amount, int unitCap) {
+
+        final long amountSpawned = spawned.stream()
+                .filter(unit -> !unit.dead())
+                .count();
+
+        if (amountSpawned == 0) {
+            final String message = String.format(INVALID_SPAWN_ERROR, PREFIX, type.name);
+            admin.sendMessage(message);
+            return;
+        }
+
+        final int unitTotalCount = team.data().countType(type);
+
+        // The unit was spawned correctly. (even if not all)
+        final String message;
+        if (amountSpawned != amount) {
+            message = String.format(UNIT_NOT_ALL_SPAWNED_MESSAGE, PREFIX, team.color, type.name, amountSpawned, amount, unitTotalCount, unitCap);
+        } else {
+            final String plural = amount == 1 ? "" : "s";
+            message = String.format(UNIT_SPAWNED_MESSAGE, PREFIX, team.color, type.name, amount, plural, unitTotalCount, unitCap);
+        }
+        admin.sendMessage(message);
+    }
+
+    private void spawnUnit(UnitType type, Player admin, int amount, Team team, float shield) {
+
+        final int unitCap = Units.getCap(team);
+        final int unitsAlive = team.data().countType(type);
+        // I spawn the maximum possible units if the cap limit is exceeded.
+        if (amount > unitCap && !Vars.state.isPaused()) amount = unitCap - unitsAlive;
+
         // I check the pause state since it allows to spawn units over the cap. (Mindustry Bug)
-        if (type.useUnitCap && unit.count() == unit.cap() && unit.dead() && !Vars.state.isPaused()) {
-            final String message = String.format(UNIT_CAP_REACHED_MESSAGE, PREFIX, unit.cap(), type.name);
+        if (type.useUnitCap && unitCap <= unitsAlive && !Vars.state.isPaused()) {
+            final String message = String.format(UNIT_CAP_REACHED_MESSAGE, PREFIX, unitCap, type.name);
             Call.sendMessage(message);
             return;
         }
 
+        // I try to spawn the units.
+        final ArrayList<Unit> spawned = new ArrayList<>();
+        for (int i = 0; i < amount; i++) {
+
+            final Unit unit;
+            // To spawn the unit even in case there are no cores. (The unit cap might be a little messed up though)
+            // An exception is the crux team.
+            if (!team.equals(Team.crux) && team.data().noCores()) {
+                unit = type.spawn(admin.team(), admin.x(), admin.y());
+                unit.team(team);
+            } else unit = type.spawn(team, admin.x(), admin.y());
+
+            unit.shield(shield == 0 ? unit.shield() : shield);
+            spawned.add(unit);
+        }
+        // I copy the value for the lambda.
+        final int finalAmount = amount;
         // I wait the next game-tick, so I can see if the unit has been killed because of a bad spawn tile.
-        Util.executeAtNextTick(() -> {
+        Util.executeAtNextTick(() -> sendSpawnMessage(spawned, admin, type, team, finalAmount, unitCap));
+    }
 
-            if (unit.dead()) {
-                final String message = String.format(INVALID_SPAWN_ERROR, PREFIX, type.name);
-                admin.sendMessage(message);
-                return;
-            }
-
-            // The unit was spawned correctly.
-            final String message = String.format(UNIT_SPAWNED_MESSAGE, PREFIX, type.name, unit.count(), unit.cap());
-            admin.sendMessage(message);
-        });
+    private Optional<UnitType> findUnitType(String unitName) {
+        // I search the wanted unit.
+        for (UnitType type : availableUnits) {
+            if (!type.name.equalsIgnoreCase(unitName)) continue;
+            return Optional.of(type);
+        }
+        return Optional.empty();
     }
 
     private void commandAction(String[] args, Player player) {
@@ -93,7 +182,9 @@ public class SpawnUnitCommand implements Command {
         }
 
         // I do this to avoid having problems with colors later on.
-        args[0] = Strings.stripColors(args[0]);
+        for (int i = 0; i < args.length; i++) {
+            args[i] = Strings.stripColors(args[i]);
+        }
 
         // I don't think there will ever be a unit called "list".
         if (args[0].equalsIgnoreCase("list")) {
@@ -101,20 +192,31 @@ public class SpawnUnitCommand implements Command {
             return;
         }
 
-        // I search the wanted unit.
-        for (UnitType type : availableUnits) {
-            if (!type.name.equalsIgnoreCase(args[0])) continue;
-            spawnUnit(type, player);
+        // If not provided 1, else the provided amount.
+        final int amount = 1 < args.length ? getAmountFromInput(args[1], player) : 1;
+        if (amount == -1) return; // Not a valid amount.
+
+        // If not provided admin team, else the provided team.
+        final Optional<Team> team = 2 < args.length ? findTeam(args[2]) : Optional.of(player.team());
+        if (team.isEmpty()) {
+            final String message = String.format(INVALID_TEAM_ERROR, PREFIX, args[2]);
+            player.sendMessage(message);
             return;
         }
 
-        final String message = String.format(UNIT_NOT_FOUND_ERROR, PREFIX, args[0]);
-        player.sendMessage(message);
+        // If not provided 0, else the provided shield.
+        final float shield = 3 < args.length ? getShieldFromInput(args[3], player) : 0;
+        if (shield == -1) return; // Not a valid shield value.
+
+        findUnitType(args[0]).ifPresentOrElse(type -> spawnUnit(type, player, amount, team.orElseThrow(), shield), () -> {
+            final String message = String.format(UNIT_NOT_FOUND_ERROR, PREFIX, args[0]);
+            player.sendMessage(message);
+        });
     }
 
     @Override
     public void registerClientCommands(CommandHandler handler) {
-        handler.register("spawn", "<list/unit>", "Spawns a unit.", this::commandAction);
+        handler.register("spawn", "<list/unit> [amount] [team] [shield]", "Spawns a unit.", this::commandAction);
     }
 
     /**
